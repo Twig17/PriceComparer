ee4b3c2b491d798e4115fadff16e25295cfc393e
Twig17 <Nick650@optonline.net>
2017-08-08 22:57:10 -0400

    add date and append to csv file

--- priceComparer.py
+++ priceComparer.py
@@ -1,11 +1,21 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 import time
+import os
 
 myUrl = 'https://www.newegg.com/Product/Product.aspx?Item=N82E16823129045'
 
 myUrlNoId = 'https://www.newegg.com/Product/Product.aspx?Item='
 myProductIdList = ["N82E16814137142", "N82E16823129045"]
+
+filename = "productInfo.csv"
+if os.path.exists(filename):
+	f = open(filename, 'a')
+else:
+	f = open(filename, 'w')
+	headers = "ProductId,Price,Date,Name\n"
+	f.write(headers)
+
 
 for productId in myProductIdList:
 	try:
@@ -17,13 +27,6 @@ for productId in myProductIdList:
 		#get the html from the url
 		pageSoup = soup(pageHtml, "html.parser")
 
-		filename = "productInfo.csv"
-		f = open(filename, "w")
-		headers = "ProductId,Name,Price\n"
-		f.write(headers)
-
-		
-
 		nameDescContainer = pageSoup.body.findAll("h1", {"id":"grpDescrip_h"})
 		nameDesc = nameDescContainer[0].text.strip()
 
@@ -33,8 +36,8 @@ for productId in myProductIdList:
 		print("Price: " + price)
 		time.sleep(5)
 
-		f.write(productId + "," + nameDesc.replace(",", "|") + "," + price + "\n")
+		f.write(productId + "," + price.replace(",", "") + "," + time.strftime("%d/%m/%Y") + "," + nameDesc.replace(",", "|") + "\n")
 	except AttributeError:
 		pass
 
-	f.close()
+f.close()
